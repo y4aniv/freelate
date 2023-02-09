a8cb30a6056c818cf9690c01975b76fbdeac9b26
Yaniv <120222659+y4aniv@users.noreply.github.com>
2023-02-09 11:52:19 +0000

    Fix PWA

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,8 @@ export default function Document () {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#FFFFFF" />
+          <meta name="mobile-wep-app-capable" content="yes"></meta>
+          <meta name="apple-mobile-wep-app-capable" content="yes"></meta>
         </Head>
         <body>
           <Main />
