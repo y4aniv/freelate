b70d7139cfb0b556647a6925a561f816ae932688
Yaniv <120222659+y4aniv@users.noreply.github.com>
2023-02-09 11:46:42 +0000

    Ajout PWA

--- next.config.js
+++ next.config.js
@@ -5,4 +5,9 @@ const withPWA = require('next-pwa')({
 
 module.exports = withPWA({
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
 })
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="fr">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default function Document () {
+
+    return (
+      <Html lang="fr">
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#ED64A6" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
 }
